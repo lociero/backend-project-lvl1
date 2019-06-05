83010acf50691721bb04c7a269267562ec112dbb
Dmitriy Frolov <omgoole@ya.ru>
2019-06-05 09:39:00 +0500

    first commit test

---
+++ Makefile
@@ -0,0 +1,4 @@
+install:
+	npm install
+start:
+	npx babel-node /mnt/e/learningJS/hexletProjects/BrainGames/src/bin/brain-games.js
---
+++ src/bin/brain-games.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const sayHi = () => {
+  const string = 'Welcome to the Brain Games';
+  console.log(string);
+};
+
+sayHi();
