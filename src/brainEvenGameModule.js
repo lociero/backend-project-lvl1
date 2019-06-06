68a516129107cd58311103c04041c3fe7e4fcb62
Dmitriy Frolov <omgoole@ya.ru>
2019-06-06 21:42:05 +0500

    fixed linter

--- src/brainEvenGameModule.js
+++ src/brainEvenGameModule.js
@@ -2,10 +2,11 @@
 import readlineSync from 'readline-sync';
 
 export const showRules = () => console.log('Answer "yes" if number even otherwise answer "no".');
+
 const getRandomInt = max => Math.floor(Math.random() * max);
 const isEven = num => num % 2 === 0;
 
-let userNameGlobal; // not sure how to do it without global var
+let userNameGlobal; // not sure how to do it without global var (make wrapper function?)
 export const askUserName = () => {
   const userName = readlineSync.question('What is your name? ');
   userNameGlobal = userName;
@@ -14,19 +15,17 @@ export const askUserName = () => {
 
 export const startGame = () => {
   const iter = (counter) => {
-    if (counter === 4) {
-      console.log(`Congratulations, ${userNameGlobal}!`);
-      return;
+    if (counter > 3) {
+      return console.log(`Congratulations, ${userNameGlobal}!`);
     }
     const value = getRandomInt(100);
-    const yourAnswer = readlineSync.question(`Question: ${value}\nYour answer: `);
+    const userAnswer = readlineSync.question(`Question: ${value}\nYour answer: `);
     const correctAnswer = isEven(value) ? 'yes' : 'no';
-    if (yourAnswer === correctAnswer) {
-      console.log('Correct!');
-      // eslint-disable-next-line
-      return iter(counter + 1);
+    if (userAnswer !== correctAnswer) {
+      return console.log(`'${userAnswer}' is wrong answer =(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userNameGlobal}!`);
     }
-    console.log(`'${yourAnswer}' is wrong answer =(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userNameGlobal}!`);
+    console.log('Correct!');
+    return iter(counter + 1);
   };
   return iter(1);
 };
