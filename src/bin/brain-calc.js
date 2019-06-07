0cae4e315fe4c372632855e649cce539c3184d7b
Dmitriy Frolov <omgoole@ya.ru>
2019-06-07 15:42:43 +0500

    added new game brain-calc

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import welcome from '../welcome';
 import askUserName from '..';
-import { showRules, startGame } from '../games/brain-even-module';
+import { showRules, startGame } from '../games/brain-calc-module';
 
 welcome();
 showRules();
