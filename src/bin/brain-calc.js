b060822dd0281465cccf9400f312d27e02967348
Dmitriy Frolov <omgoole@ya.ru>
2019-06-11 09:27:29 +0500

    brain-calc optimized

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-import welcome from '../welcome';
-import askUserName from '..';
-import { showRules, brainCalc } from '../games/brain-calc-module';
-import startGame from '../startGame-module';
+import startGame from '../games/calc';
 
-welcome();
-showRules();
-startGame(askUserName(), brainCalc);
+startGame();
---
+++ src/games/calc.js
@@ -0,0 +1,38 @@
+import { getRandomInt, startGame } from '..';
+
+const rules = 'What is the result of the expression?\n';
+
+const operations = {
+  1: '+',
+  2: '-',
+  3: '*',
+};
+
+const calculate = (num1, num2, operation) => {
+  switch (operation) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return console.log('undefined operation');
+  }
+};
+
+const brainCalc = () => {
+  const num1 = getRandomInt(0, 10);
+  const num2 = getRandomInt(0, 10);
+  const operation = operations[getRandomInt(1, 3)];
+
+  const correctAnswer = calculate(num1, num2, operation);
+  const question = `${num1} ${operation} ${num2}`;
+
+  return {
+    answer: correctAnswer,
+    question,
+  };
+};
+
+export default () => startGame(rules, brainCalc);
