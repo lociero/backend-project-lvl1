e832746608651b1cdbcce80fe6bb3bb547b5e83c
Dmitriy Frolov <omgoole@ya.ru>
2019-06-12 11:02:25 +0500

    optimized isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,7 +6,7 @@ const isPrime = (num) => {
   if (num <= 1) {
     return false;
   }
-  for (let div = 2; div < num; div += 1) {
+  for (let div = 2; div < num / 2; div += 1) {
     if (num % div === 0) {
       return false;
     }
