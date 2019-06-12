d6a8102878dc96466983fa3a71879c77ab99b563
Dmitriy Frolov <omgoole@ya.ru>
2019-06-12 11:33:02 +0500

    used destructuring for quest and answer

--- src/index.js
+++ src/index.js
@@ -11,9 +11,8 @@ export default (description, initGame) => {
     if (gameRound === 4) {
       return console.log(`Congratulations, ${userName}!`);
     }
-    const initializedGame = initGame();
-    const correctAnswer = initializedGame.answer;
-    const userAnswer = readlineSync.question(`Question: ${initializedGame.question}\nYour answer: `);
+    const { answer: correctAnswer, question } = initGame();
+    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
     if (userAnswer !== String(correctAnswer)) {
       return console.log(`'${userAnswer}' is wrong answer =(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
     }
