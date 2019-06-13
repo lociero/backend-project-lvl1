df4bea55d5c0f762c9908bd48fc483ad49a6eaeb
Dmitriy Frolov <omgoole@ya.ru>
2019-06-13 17:45:55 +0500

    deleted brain-games modules, fixed package.json

--- src/games/brain-games.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export default () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('What is your name? ');
-  console.log(`Hi, ${userName}!\n`);
-};
