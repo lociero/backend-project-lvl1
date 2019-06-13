ee69dab80753d53f65fcb3bc78dc4c2e693dc06a
Dmitriy Frolov <omgoole@ya.ru>
2019-06-13 18:19:25 +0500

    refactored progression module

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,24 +3,22 @@ import getRandomInt from '../utils';
 
 const description = 'What number is missing in the progression?';
 
+const generateProgression = (step, acc, length) => {
+  if (acc.length === length) {
+    return acc;
+  }
+  const lastElement = acc[acc.length - 1];
+  const newAcc = acc.concat(lastElement + step);
+  return generateProgression(step, newAcc, length);
+};
+
 const generateData = () => {
   const lengthOfProgression = 10;
+  const stepSize = getRandomInt(1, 5);
+  const startNumber = getRandomInt(0, 10);
 
-  const getRandomProgression = (length = 10) => {
-    const iter = (step, acc) => {
-      if (acc.length === length) {
-        return acc;
-      }
-      const newAcc = acc.concat(acc[acc.length - 1] + step);
-      return iter(step, newAcc);
-    };
-    const stepSize = getRandomInt(1, 5);
-    const startNumber = getRandomInt(0, 10);
-    return iter(stepSize, [startNumber]);
-  };
-
-  const progression = getRandomProgression(lengthOfProgression);
-  const indexForChange = getRandomInt(1, progression.length - 2);
+  const progression = generateProgression(stepSize, [startNumber], lengthOfProgression);
+  const indexForChange = getRandomInt(1, lengthOfProgression - 2);
   const correctAnswer = progression[indexForChange];
   progression[indexForChange] = '..';
   const question = progression.join(' ');
