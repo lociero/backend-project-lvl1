be31f1206cdd9a677461308179300a677073eb0f
Dmitriy Frolov <omgoole@ya.ru>
2019-06-14 15:37:01 +0500

    fixed prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,8 +7,8 @@ const isPrime = (num) => {
   if (num <= 1) {
     return false;
   }
-  for (let div = 2; div <= num / 2; div += 1) {
-    if (num % div === 0) {
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
       return false;
     }
   }
